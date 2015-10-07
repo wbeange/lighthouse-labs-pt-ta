067c32d2aa9a6af7b51f82757f836100865e8411
= <=>
2015-10-07 04:04:24 +0000

    W6D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,8 @@
+#
+# Lighthouse Labs Web Fundamentals Course
+# basic web project - movie app
+#
+
 helpers do
   def current_user
     @current_user = User.find_by(:id => session[:user_id]) if session[:user_id]
@@ -17,6 +22,10 @@ get '/' do
   erb :index
 end
 
+#
+# Sessions Controller
+#
+
 get '/login' do
   erb :login
 end
@@ -29,7 +38,7 @@ post '/login' do
 
   if user && user.authenticate(password)
     session[:user_id] = user.id
-    redirect '/'
+    redirect '/users/self'
   else
     redirect '/login'
   end
@@ -40,6 +49,10 @@ post '/logout' do
   redirect '/login'
 end
 
+#
+# Users Controller
+#
+
 get '/users/new' do
   erb :user_new
 end
@@ -49,8 +62,8 @@ post '/users' do
   email = params[:email].downcase
   password = params[:password]
 
-  # halt 400, "Email can't be blank" if email.blank?
-  # halt 409, "Email already exists" if User.exists?(:email => email)
+  halt 400, "Email can't be blank" if email.blank?
+  halt 409, "Email already exists asdf" if User.exists?(:email => email)
 
   user = User.create(:username => username, :email => email, :password => password)
 
@@ -69,13 +82,12 @@ get '/users/:id' do
   erb :user
 end
 
+#
+# Movies Controller
+#
+
 get '/movies/new' do
   erb :movie_new
-end
-
-get '/movies' do
-  @movies = Movie.where(:user_id => current_user.id)
-  erb :movies
 end
 
 post '/movies' do
@@ -83,5 +95,37 @@ post '/movies' do
 
   halt 400, movie.errors.full_messages.join(',') unless movie.valid?
 
-  redirect '/movies'
+  redirect "/movies/#{movie.id}"
+end
+
+get '/movies' do
+  @movies = Movie.where(:user_id => current_user.id)
+  erb :movies
+end
+
+get '/movies/:id' do
+  @movie = Movie.find(params[:id])
+  @reviews = Review.where(:movie_id => params[:id])
+  erb :movie
+end
+
+#
+# Reviews Controller
+#
+
+get '/movies/:movie_id/reviews/new' do
+  @movie = Movie.find(params[:movie_id])
+  erb :review_new
+end
+
+post '/movies/:movie_id/reviews' do
+  movie = Movie.find(params[:movie_id])
+
+  movie.reviews.create(
+    :title => params[:title],
+    :description => params[:description],
+    :rating => params[:rating]
+  )
+
+  redirect "/movies/#{movie.id}"
 end
